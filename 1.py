9701587579dddf21f24e10dec8ad2d252890cc4a
Andy Huang <andy.huang@morningstar.com>
2018-04-03 19:36:29 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# python project for andy 
+# Author:'ahuang3'
+# Time: 2018/4/3, 13:02
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.rand(1,10)
+y = np.random.randn(1,10)
+
+plt.plot(x)
+plt.plot(y)
+
+plt.show()
+print (x)
+#TODO
+
